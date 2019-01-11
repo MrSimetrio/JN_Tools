400e71118d074d41a70fb60b20e488f577f8cc12
江鸟 <fang@jiangniao-MBP.lan>
2019-01-11 09:58:44 +0800

    创建项目

---
+++ Tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tools
+//
+//  Created by 江鸟 on 2019/1/11.
+//  Copyright © 2019年 Simetrio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tools
+//
+//  Created by 江鸟 on 2019/1/11.
+//  Copyright © 2019年 Simetrio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
